23afb6eb5f1c6927883675a6de81b183a4944e6a
buri <buri@aims.edu.gh>
2019-02-01 14:32:46 +0200

    First commit for the senescence model including fitting procedure.X

---
+++ senescence_model.R
@@ -0,0 +1,83 @@
+#Model for immunoscenesense via hayflick limit
+#Author: Gershom Buri
+#Date: 14th Jan 2019
+#*******************************************************************************
+library(parallel)
+library("minpack.lm")
+
+
+#******************************  functions **********************************
+
+#Implementing the Prod symbol in the equation f_t
+mltpy <- function(j, x, beta, h, n){
+  #pause(0.1)
+  total <- prod(sapply(1:j,function(k) 2 * beta * h ^n/ (h^n + (x-k)^n)))
+  return(total)
+}
+
+#function for the fraction of T cells with x divisions at time t; f_t
+f <- function(x, t, beta){
+  #experimental parameters: delta = turn over rate (per day), C = initial number
+  # at t = 0  
+  delta = 0.0015; h = 50; n = 6; C = 0.0017; a = 1/(33.33 * 12); mu <- 9
+  d <- 1 - beta - delta
+  #sum of f_0's
+  total1 <- dpois(x, mu)*(1 + C / d * sum(sapply(0:x, 
+         function(i) exp(-a * i) *  d^(- i)))) 
+  
+  total2 <-  total1 +  sum(sapply(1:x, function(j) d^(-j) * dpois(x - j, mu) * 
+          mltpy(j, x, beta, h, n) *  (choose(t,j) +  C / d * 
+        sum(sapply(0:(x - j), function(i) choose(x - i, j) * 
+                     exp(-a * i) * d^(-i))))))
+  
+    return(d^(t) * total2)
+}
+
+#simulating the distribution of cell divisions (0 - 100) at t = 25000 days, 
+#for beta = 0.0015,
+system.time(coll <- sapply(0:100,f,25000,0.0015))
+plot(1:length(coll[!is.na(coll)]),coll[!is.na(coll)]/sum(coll[!is.na(coll)]),
+     type = "l", xlab = "No. of cell divisions", ylab = "Probability")
+
+#Calculating aging 
+getPred <- function(t, beta){
+  #Predictions at time point t
+  coll <- sapply(0:100, f, t = t*365, beta)
+  #normalising
+  f_t <- coll/sum(coll)
+  #summing f_t >= h
+  return(sum(f_t[50:100]))
+}
+
+#residual function for fitting; based on log-transformed values
+residFun <- function(par, observed, yr){
+  cl <- makeCluster(3)
+  clusterExport(cl, c("f","mltpy", "getPred", "par", "t"))
+  prdctns <- parSapply(cl, (yr - 20), FUN = getPred, par$beta)
+  stopCluster(cl)
+  #prdctns <- sapply((t - 20), getPred, par$beta)
+  resids <- log10(par$M * prdctns) - log10(observed)#par$M * prdctns - observed#
+  return(ifelse(is.nan(resids), 1e6, resids))
+}
+
+#********************************** data load ********************************
+mydata <- read.csv("/home/buri/Documents/incidenceData.csv")
+incidenceData <- mydata[-(1:5),c(2,3)]
+incidenceData$Age <- seq(22,90,5)
+View(incidenceData)
+
+#********************************* data fitting ******************************
+
+inits <- list(beta = fit$par$beta, M = fit$par$M)
+fit <- nls.lm(par = inits, lower = c(0, 0), upper = c(0.001,1e23),
+                          fn = residFun, observed = incidenceData$Rate.per.100.000,
+                          t = incidenceData$Age,
+                          control = nls.lm.control(nprint=1))#,  ftol = 1e-4
+
+#It.    5, RSS =  0.0347676, Par. =  3.10975e-05  1.88351e+20
+
+prdctns <- sapply((incidenceData$Age - 20), getPred, fit$par$beta)
+plot(incidenceData$Age,prdctns*fit$par$M)
+
+plot(incidenceData$Age, log10(prdctns*fit$par$M))
+plot(incidenceData$Age, log10(incidenceData$Rate.per.100.000))
